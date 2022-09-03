c2298fd57a86c1314a9d521d1ca86c126aa54855
azoozalharte <zezo50115@protonmail.com>
2022-09-04 01:26:47 +0300

    fixing bug when edit the note, note sty at the same index

--- js/edit-note.js
+++ js/edit-note.js
@@ -1,20 +1,21 @@
 const noteId = location.hash.substr(1);
 const notes = savedNotes();
 
-const note = notes.filter((note) => {
-  return note.id === noteId;
-});
+const noteIndex = notes.findIndex((note) => note.id === noteId);
 
 // fill forms with data
-document.querySelector("input").value = note[0].title;
-document.querySelector("textarea").value = note[0].body;
+document.querySelector("input").value = notes[noteIndex].title;
+document.querySelector("textarea").value = notes[noteIndex].body;
 
 document.querySelector("#edit-form").addEventListener("submit", function (e) {
   e.preventDefault();
   removeNote(noteId);
-  note[0].title = e.target.title.value;
-  note[0].body = e.target.cardBody.value;
-  notes.push(note[0]);
+
+  notes.splice(noteIndex, 0, {
+    id: noteId,
+    title: e.target.title.value,
+    body: e.target.cardBody.value,
+  });
   localStorage.setItem("notes", JSON.stringify(notes));
   location.assign("/index.html");
 });
