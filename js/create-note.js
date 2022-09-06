9928bfef80a659c4ec02ec4a9d54bae468b4c75a
azoozalharte <zezo50115@protonmail.com>
2022-09-06 15:37:48 +0300

    adding time when notes created

--- js/create-note.js
+++ js/create-note.js
@@ -4,10 +4,14 @@ const notes = savedNotes();
 document.querySelector("#note-form").addEventListener("submit", function (e) {
   e.preventDefault();
 
+  const time = moment().valueOf();
+
   notes.push({
     id: noteId,
     title: e.target.title.value,
     body: e.target.cardBody.value,
+    createdAt: time,
+    updatedAt: time,
   });
   localStorage.setItem("notes", JSON.stringify(notes));
   location.assign("/index.html");
